7c82484d71923d9ec1718cbffbef6a13b677d03a
vishalicious213 <vishalicious213@gmail.com>
2019-08-22 18:44:13 -0400

    answered four principles in THIS file

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,17 @@
-/* The for principles of "this";
-* in your own words. explain the four principle for the "this" keyword below.
+/* The four principles of "this";
+* in your own words. explain the four principles for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window Binding: Global scope, shows the window/console object. 
+     Shows entire JS language in Inspector. Don't use this!
+
+* 2. Implicit Binding: Most common. Automatically happens. Refers to
+     object to left of the dot.
+
+* 3. Explicit Binding: Overrides another binding. Explicitly points
+     to another object. Uses .call, .apply & .bind.
+
+* 4. New Binding: Uses "new" keyword. Constructs a new object and
+     points THIS to it.
 *
 * write out a code example of each explanation above
 */
